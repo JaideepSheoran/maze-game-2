e3f15d3d024e22142b9417bfa56bd013903f81f3
JaideepSheoran <jaideepsinghsheoran@gmail.com>
2022-04-06 21:01:50 +0530

    test

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.static("./public"));
-//
-//app.get("/", (req, res)=>{
-//    res.send('Hello ;)');
-//})
 
 app.listen(port, ()=>{
     console.log(`Activated at ${port}`);
